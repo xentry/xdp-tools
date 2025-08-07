a5a02b20440227df2cf944e9511ad2cf2cb23803
Vlad Rubtsov <axentry@gmail.com>
2025-08-07 10:49:30 +0300

    Move util code into xdp-trafficgen

---
+++ xdp-trafficgen/util/util.mk
@@ -0,0 +1,8 @@
+UTIL_OBJS := params.o logging.o util.o stats.o xpcapng.o
+UTIL_BPF_OBJS :=
+
+ifneq ($(BPFTOOL),)
+UTIL_OBJS += xdp_sample.o
+UTIL_BPF_OBJS += xdp_sample.bpf.o xdp_load_bytes.bpf.o
+endif
+
